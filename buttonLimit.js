b869a9dce420a0e44d0d68a8bbb6f0051d2dbd8c
Dominic <dominic.sangcap@gmail.com>
2023-07-09 09:52:05 -0700

    add user view of entry count they are able to add

--- buttonLimit.js
+++ buttonLimit.js
@@ -1,10 +1,14 @@
 //limit times user can hit submit Button for dB text
 var add_dB_Button = document.getElementById('add_dB');
 var addCount = 0; //might have to reference body of input later in future when/if i add functionality to import export data
-var maxCount = 3; //allows for 3 entries
+var maxCount = 5; //allows for 5 entries
+//new stuff, update counter to show max amount
+var updateCounter = document.getElementById('update_counter');
 
 add_dB_Button.addEventListener("click", () => {
     addCount++;
+    //update max count from user input
+    updateCounter.innerHTML++;
     console.log("Add Count = " + addCount);
     if (addCount > maxCount) {
         //might have to disable this in the future???
--- buttonLimit2.js
+++ buttonLimit2.js
@@ -1,10 +1,14 @@
 //limit times user can hit submit Button for oC's text
 var add_dB_Button2 = document.getElementById('add_oC');
 var addCount2 = 0; //might have to reference body of input later in future when/if i add functionality to import export data
-var maxCount2 = 3; //allows for 3 entries
+var maxCount2 = 5; //allows for 5 entries
+//new stuff, update counter to show max amount
+var updateCounter2 = document.getElementById('update_counter2');
 
 add_dB_Button2.addEventListener("click", () => {
     addCount2++;
+    //update max count from user input
+    updateCounter2.innerHTML++;
     console.log("Add Count = " + addCount2);
     if (addCount2 > maxCount2) {
         //might have to disable this in the future???
