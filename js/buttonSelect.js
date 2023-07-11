488ba817a9a689cd5b93f4b009ac634562b20625
Dominic <dominic.sangcap@gmail.com>
2023-07-10 17:58:04 -0700

    possibly figured out dynamic reference

--- js/buttonSelect.js
+++ js/buttonSelect.js
@@ -4,6 +4,25 @@ function buttonSelect() {
   var outerDiv = document.getElementById('outerDiv');
   var innerDiv = outerDiv.querySelector('#innerDiv');
   var form = innerDiv.querySelector('#testForm');
+  //new code to refernce
+  var selectedOption = form.querySelector('input[name="option"]:checked');
+
+  if (selectedOption) {
+      var selectedValue = selectedOption.value;
+    console.log("Selected button: " + selectedValue);
+  } else {
+    console.log("No button selected.");
+  }
+  
+}
+
+/*og buttonSelect code
+//reference to button object rhough mutlipe element
+function buttonSelect() {
+  //div-div-form-amongst inut[radio]
+  var outerDiv = document.getElementById('outerDiv');
+  var innerDiv = outerDiv.querySelector('#innerDiv');
+  var form = innerDiv.querySelector('#testForm');
   var radioBtn = form.querySelectorAll('.radio-form');
   var selectedOption;
 
@@ -21,3 +40,4 @@ function buttonSelect() {
   }
   
 }
+*/
--- js/buttonSelect2.js
+++ js/buttonSelect2.js
@@ -1,15 +1,30 @@
 //reference to button object rhough mutlipe element
 function buttonSelect2() {
+    /*static reference
     //div-div-form-amongst inut[radio]
     var outerDiv = document.getElementById('outerDiv');
     var innerDiv = outerDiv.querySelector('#innerDiv');
     var form = innerDiv.querySelector('#testForm');
-  
     //new code to refernce
-    var selectedOption = form.querySelector('input[name="option"]:checked').value;
+    var selectedOption = form.querySelector('input[name="option"]:checked');
   
     if (selectedOption) {
-      console.log("Selected button: " + selectedOption);
+        var selectedValue = selectedOption.value;
+      console.log("Selected button: " + selectedValue);
+    } else {
+      console.log("No button selected.");
+    }
+    */
+    //div-div-form-amongst inut[radio]
+    var outerDiv = document.getElementById('todo-lane');
+    var innerDiv = outerDiv.querySelector('#testDynam0');
+    var form = innerDiv.querySelector('#form-0');
+    //new code to refernce
+    var selectedOption = form.querySelector('input[name="option0"]:checked');
+  
+    if (selectedOption) {
+        var selectedValue = selectedOption.value;
+      console.log("Selected button: " + selectedValue);
     } else {
       console.log("No button selected.");
     }
