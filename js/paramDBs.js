add60c9a40a6f9dcc209a8e59404b731932aa673
Dominic <dominic.sangcap@gmail.com>
2023-07-12 23:33:23 -0700

    able to mirror getOC and getDB onto copy 2nd user

--- js/paramDBs.js
+++ js/paramDBs.js
@@ -1,13 +1,14 @@
-function paramDBs() {
-    var outerDiv = document.getElementById('todo-lane');
+//getDBs, but use parameters to pass what makes unique iD number ex. todo-lane(2), pass the 2
+function paramDBs(x_od, y_id, f_id, i_name) {
+    var outerDiv = document.getElementById('todo-lane' + x_od);
     let size = outerDiv.getElementsByTagName('p').length;
     console.log('count of paragraphs in outerDiv: '+ size);
 
     for (var i = 0; i < size; i++) {
-        var innerDiv = outerDiv.querySelector('#testDynam' + i);
-        var form = innerDiv.querySelector('#form-' + i);
+        var innerDiv = outerDiv.querySelector('#testDynam' + y_id + i);
+        var form = innerDiv.querySelector('#form' + f_id + '-' + i);
         //new code to refernce, ew to the numerical string refernce; based on <input> where button name="" is option + i, the selected one
-        var selectedOption = form.querySelector('input[name="option' + i + '"]:checked');
+        var selectedOption = form.querySelector('input[name="' + i_name + i + '"]:checked');
       
         //make sure user actually selected a value
         if (selectedOption) {
--- js/paramOCs.js
+++ js/paramOCs.js
@@ -1,3 +1,20 @@
-function paramOCs() {
-    
+//getOCs, but use parameters to pass what makes unique iD number ex. todo-lane(2), pass the 2
+function paramOCs(x_od, y_id, i_id) {
+    var outerDiv = document.getElementById('todo-lane'+ x_od);
+    let size = outerDiv.getElementsByTagName('p').length;
+    console.log('count of paragraphs in outerDiv: '+ size);
+
+    for (var i = 0; i < size; i++) {
+        var innerDiv = outerDiv.querySelector('#testDynam'+ y_id + i);
+        var selectedValue = innerDiv.querySelector('#uniqueID' + i_id + i).value;
+
+        if (selectedValue >= 0 && selectedValue <= 10) {
+            //get input value from paragraph
+            var pElement = outerDiv.getElementsByTagName('p')[i].textContent;
+            console.log(i + ". |User input: " + pElement + ", Rangevalue: " + selectedValue);   
+        } else {
+            //this should almost never happen, because default value is 5. idk how user could input < 0 or > 10
+            console.log("No Range selected/changed.");
+        }
+    } 
 }
