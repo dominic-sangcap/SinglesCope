0f8b419a121b19fcae55067c393b8d8d169995d2
Dominic <dominic.sangcap@gmail.com>
2023-07-15 20:02:15 -0700

    attempting getValueAt functinoality,  ight scrape

--- js/twoUsersData.js
+++ js/twoUsersData.js
@@ -45,41 +45,31 @@ class userData {
 
         let output = '';
         output += "<br>"; //aestheics fo website
-        //og gpt prompt
-        /*
-        this.userData.forEach((data, index) => {
-            output += `User ${index + 1}: `;
-            data.forEach((item) => {
-                output += `${item}, `;
-            });
-            output = output.slice(0, -2); // Remove the extra comma and space
-            output += "<br>";
-        });
-        */
 
         //my prompt
         let index = 0;
         for(let header of formatting) {
             output += header + "<br>";
-            for(let key of this.userData[index]) {
+            for(let item of this.userData[index]) {
                 //honestly this refernce is cooked, and im brute forcing it
-                //
-                output += "->" + key[0] + ": "+ key[1] + "<br>";
+                output += "->" + item[0] + ": "+ item[1] + "<br>";
             }
             output += "<br>";
+            index++;
         }
 
         outputElement.innerHTML = output;
     }
 
-    //write to html, testing IP
-    getValueAt(outerIndex, innerIndex) {
+    //write to html, testing IP using output1, so i can make sure other stuff works
+    //NOte** might have to really think about the structure of how elements stored
+    getValueAt(outerIndex, arrayItem) {
         //output element??
-        const outputElement = document.getElementById('output');
-        const value = userData.getIndividualValue(outerIndex, innerIndex);
-      
-        if (value) {
-          outputElement.textContent = `Value at userData[${outerIndex}][${innerIndex}]: ${value}`;
+        const outputElement = document.getElementById('output1');
+        const value = arrayItem[1];
+        
+        if (value != null) {
+          outputElement.textContent = `Value at userData[${outerIndex}][${arrayItem[0]}]: ${arrayItem[1]}`;
         } else {
           outputElement.textContent = `Invalid index provided.`;
         }
@@ -90,16 +80,16 @@ class userData {
 
 //the actual implementation of storing data and referencing it
 function twoUsersData() {
-    console.log("in twoUserData");
+    //console.log("in twoUserData");
     let user1 = new userData();
     let user2 = new userData();
 
+    //test input for user1
     let testData1 = {
         "doesnt like boba": "dealbreaker",
         "flat earther": "dealbreaker",
         "ugly af": "dealbreaker",
     }
-
     let testData2 = {
         "sense of humor": 7,
         "has a birthday holiday": 10,
@@ -111,28 +101,35 @@ function twoUsersData() {
     for(let key in testData1) {
         let value = testData1[key];
         user1.setValue1(key, value);
-        console.log("key: " + key + ", value: " + value);
+        //in user1, this pushes a [key, value] into user1[0]
+        //console.log("key: " + key + ", value: " + value);
     }
     //console.log("init twoUserData DB");
 
     for(let key2 in testData2) {
         let value2 = testData2[key2];
         user1.setValue2(key2, value2);
-        console.log("key2: " + key2 + ", value2: " + value2);
+        //in user1, this pushes a [key, value] into user1[1]
+        //console.log("key2: " + key2 + ", value2: " + value2);
     }
     //console.log("init twoUserData OC");
 
-    //TEsting getValue does NOT WORK??????
+    //Testing getValue, print to html div element
     let temp = user1.getValue();
-    console.log("return twoUserData in div under button");
-/*
+    //console.log("return twoUserData in div under button");
+
     //test .getValueaAt()
-    for(let key2 in testData2) {
-        let value2 = testData2[key2];
-        user1.setValue2(key2, value2);
-        console.log("key2: " + key2 + ", value2: " + value2);
+    console.log("Ideally this wroks 1st try: getValueat(x, y)---Test1s");
+    //let num = user1[0].length;
+   // console.log("user1[0].length: "+ num);
+    for(let i = 0; i < user1.userData.length; i++) {
+        for (let item of user1.userData[i]) {
+            //iterate over the userData[i][These [x, y] values]
+            let value = user1.getValueAt(i, item);
+            console.log("getValueAt[" + i + "][" + item[0] + "]:  " + item[1]);
+        }
     }
-*/
+
     /*
     //init user2 data
     user2.setValue1();
